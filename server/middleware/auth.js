f3a9c8c05fd174ab63a64c7180be50641befa0af
Rahul <9675rahulsingh@gmail.com>
2024-02-17 11:06:02 +0530

    first commit

---
+++ server/middleware/auth.js
@@ -0,0 +1,16 @@
+// middleware/auth.js
+
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.header('auth-token');
+  if (!token) return res.status(401).json({ message: 'Access denied' });
+
+  try {
+    const verified = jwt.verify(token, 'secretkey');
+    req.user = verified.userId;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid token' });
+  }
+};
